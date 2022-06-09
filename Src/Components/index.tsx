f01f240a8f306cf9cc2e4eeb9271456ffdd7b2d3
ClaudionorJunior <devclaudionor@gmail.com>
2022-06-08 22:14:04 -0300

    fix: cycle

--- Src/Components/index.tsx
+++ Src/Components/index.tsx
@@ -2,7 +2,6 @@ export { default as LoadingScreen } from './LoadingScreen';
 export { default as PlayerSelect } from './PlayerSelect';
 export { default as PlayerPointsDistribution } from './PlayerPointsDistribution';
 export { default as ProgressBarTitle } from './ProgressBarTitle';
-export { default as Slotitem } from './SlotItem';
 export { default as ItemsEquippedPlayer } from './ItemsEquippedPlayer';
 export { default as Inventory } from './Inventory';
 export { default as PlayerStatus } from './PlayerStatus';
